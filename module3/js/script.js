30330ea1d4e71ba82be9969b7f7dad25adc0792a
Caneppele <caneppele1.6@gmail.com>
2022-08-02 10:55:15 -0300

    esse era o +

--- module3/js/script.js
+++ module3/js/script.js
@@ -1,1 +1,15 @@
-var x = 'Hellow World';
+var x = 'HelloWorld';
+
+var verificarInvalidos = function() {
+    var retorno = false;
+    var invalidos = "!@#$%^&*()_+-=/*-+;:?/.,><{}[]`~|'" + '"';
+    for(var i = 0;i<x.length && retorno == false;i++){
+        for(var y = 0;y<invalidos.length && retorno == false;y++){
+            if(x.charAt(i) == invalidos.charAt(y)){retorno=true}
+        }
+        
+    }
+    return(retorno);
+};
+
+console.log(verificarInvalidos());
