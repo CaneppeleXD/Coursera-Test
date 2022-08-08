a2168308339872b2831f51a9d10fb3a7512a706b
Caneppele <caneppele1.6@gmail.com>
2022-08-08 17:50:12 -0300

    aaa

--- js/script.js
+++ js/script.js
@@ -14,6 +14,9 @@ $(function () {
 (function (global) {
     var dc = {};
     var homeHtml = "telas/home.html";
+    var allcategories = "data/categories.json";
+    var menu_categories = "telas/menu-categories.html"
+    var categoryHtml = "telas/categories.html"
     var insertHtml = function (selector, html) {
         var target = document.querySelector(selector);
         target.innerHTML = html;
@@ -25,6 +28,12 @@ $(function () {
 
     };
 
+    var insertProperty = function (string, propName, propValue){
+        var propToReplace = "{{"+propName+"}}";
+        string = string.replace(new RegExp(propToReplace, "g"), propValue);
+        return string;
+    }
+
     document.addEventListener("DOMContentLoaded", function (event) {
         showLoading("#main-content");
 
@@ -34,6 +43,42 @@ $(function () {
             },
             false);
     });
+
+    dc.loadMenuCategories = function(){
+        showLoading("#main-content");
+        $utilajax.sendGetRequest(allcategories, buildAndShowCategoriesHTML, true);
+    }
+
+    function buildAndShowCategoriesHTML (categories) {
+        $utilajax.sendGetRequest(
+            menu_categories,
+            function (categoriesTitleHtml){
+                $utilajax.sendGetRequest(
+                    categoryHtml,
+                    function (categoryHtml) {
+                        var categoriesViewHtml = buildCategoriesViewHtml(categories, categoriesTitleHtml, categoryHtml);
+                        insertHtml("#main-content", categoriesViewHtml)
+                    },
+                false);
+            },
+        false);
+    }
+
+    function buildCategoriesViewHtml (categories, categoriesTitleHtml, categoryHtml) {
+        var finalHtml = categoriesTitleHtml;
+        finalHtml += "<section class = 'row'>";
+
+        for(var i = 0;i < categories.length; i++){
+            var html = categoryHtml;
+            var name = "" + categories[i].name;
+            var short_name = categories[i].short_name;
+            html = insertProperty(html, "name", name);
+            html = insertProperty(html, "short_name", short_name);
+            finalHtml += html;
+        }
+        finalHtml += "</section>";
+        return(finalHtml);
+    }
 
     global.$dc = dc;
 })(window);
